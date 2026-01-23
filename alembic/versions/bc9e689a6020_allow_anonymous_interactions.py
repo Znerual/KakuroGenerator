e8da742b5d6b2d01f1a4e7b55157310088165b49
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2026-01-23 13:57:28 +0100

    Alembic upgrade

--- alembic/versions/bc9e689a6020_allow_anonymous_interactions.py
+++ alembic/versions/bc9e689a6020_allow_anonymous_interactions.py
@@ -47,7 +47,7 @@ def upgrade() -> None:
                existing_nullable=True)
         batch_op.create_index(batch_op.f('ix_puzzles_short_id'), ['short_id'], unique=True)
         batch_op.create_index(batch_op.f('ix_puzzles_template_id'), ['template_id'], unique=False)
-        batch_op.create_foreign_key(None, 'puzzle_templates', ['template_id'], ['id'])
+        batch_op.create_foreign_key('fk_puzzles_templates', 'puzzle_templates', ['template_id'], ['id'])
 
     with op.batch_alter_table('users', schema=None) as batch_op:
         batch_op.alter_column('is_admin',
@@ -68,7 +68,7 @@ def downgrade() -> None:
                existing_server_default=sa.text('0'))
 
     with op.batch_alter_table('puzzles', schema=None) as batch_op:
-        batch_op.drop_constraint(None, type_='foreignkey')
+        batch_op.drop_constraint('fk_puzzles_templates', type_='foreignkey')
         batch_op.drop_index(batch_op.f('ix_puzzles_template_id'))
         batch_op.drop_index(batch_op.f('ix_puzzles_short_id'))
         batch_op.alter_column('template_id',
